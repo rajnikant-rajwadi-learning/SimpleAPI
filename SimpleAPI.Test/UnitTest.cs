b18d0bf274ddadf40fad8b253a107d6139741dee
rajnikant-rajwadi-learning <72257866+rajnikant-rajwadi@users.noreply.github.com>
2020-10-05 06:19:25 -0700

    Initial Commit

---
+++ SimpleAPI.Test/UnitTest.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest
+    {
+        WeatherForecastController controller;
+
+
+        public UnitTest()
+        {
+            controller = new WeatherForecastController();
+        }
+
+        [Fact]
+        public void GetReturnsByName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Rajnikant Rajwadi", returnValue.Value);
+        }
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
